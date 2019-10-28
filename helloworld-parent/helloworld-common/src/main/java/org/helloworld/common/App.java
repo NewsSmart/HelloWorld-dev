396fa60ca000e87cebce66972774f0f6f3ca6c23
zhuxinwen <zhuxinwen@macbook-air>
2019-10-28 17:37:22 +0800

    tw commit

--- helloworld-parent/helloworld-common/src/main/java/org/helloworld/common/App.java
+++ helloworld-parent/helloworld-common/src/main/java/org/helloworld/common/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!dddddssdddddddd" );
-        System.out.println( "Hello World!ddddddddddddcced" );
+        System.out.println( "Hello World!dddddddddddddddcced" );
     }
 }
